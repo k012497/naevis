23ce9b576ddbfe4bd609248ec38707b3db84f70b
sophie <sophie.kim@luxrobo.com>
2021-10-03 00:22:12 +0900

    Create Header component

---
+++ src/component/Header.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { Link } from "svelte-navigator";
+</script>
+
+<header>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">home</Link>
+        </li>
+        <li>
+          <Link to="/contact">contact</Link>
+        </li>
+        <li>
+          <Link to="/project">project</Link>
+        </li>
+      </ul>
+    </nav>
+</header>
