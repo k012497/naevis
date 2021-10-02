1032e3e6a19356870f964da658f87eca9cccdbe7
sophie <sophie.kim@luxrobo.com>
2021-10-03 00:42:19 +0900

    Remove svelte-navigator

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,5 @@
 <script lang="ts">
-	import Header from "./component/Header.svelte";
+	import Header from "./components/Header.svelte";
 </script>
 
 <Header />
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,9 @@
+<header>
+    <nav>
+      <ul>
+        <li>
+          <a href="/naevis">home</a>
+        </li>
+      </ul>
+    </nav>
+</header>
